416162efab08659f5f2b4ee90f5d01de304e9c85
LDVictor <victor.borges@ccc.ufcg.edu.br>
2017-06-27 17:38:20 -0300

    Questões finalizadas.

--- questao3.hs
+++ questao3.hs
@@ -1,1 +1,18 @@
+main = do
+  input <- getLine
+  let result = verificaPrimoCasado(read input)
+  print result
+  
+verificaPrimoCasado :: Int -> String
+verificaPrimoCasado n
+	| ehPrimo(n+2) && ehPrimo(n) = "PRIMO CASADO!"
+	| ehPrimo(n) = "PRIMO!"
+	| otherwise = "NAO PRIMO!"
+  
+listaDivisores :: Int -> [Int]
+listaDivisores n = [ x | x <- [1 .. n], ((mod n x) == 0)]
+
+ehPrimo :: Int -> Bool
+ehPrimo n = if (listaDivisores n) == [1,n] then True else False
+
 
