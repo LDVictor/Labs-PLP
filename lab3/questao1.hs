f68c5432dedee35836f6b75caddbd4db7beac713
LDVictor <victor.borges@ccc.ufcg.edu.br>
2017-07-04 13:08:06 -0300

    Criando os arquivos do Lab 3.

---
+++ lab3/questao1.hs
@@ -0,0 +1,7 @@
+main = do
+  input <- getLine
+  let result = function(read input)
+  print result
+
+function :: Bool -> Bool
+function x = x
---
+++ lab3/questao3.hs
@@ -0,0 +1,9 @@
+main = do
+  input <- getLine
+  let result = function(read input)
+  print result
+
+function :: Bool -> Bool
+function x = x
+
+
