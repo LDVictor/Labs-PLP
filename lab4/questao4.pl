881b1e22d73d6918ff723c0785a3ba871eefed0a
LDVictor <victor.borges@ccc.ufcg.edu.br>
2017-08-01 19:07:02 -0300

    Lab 4 finalizado.

--- lab4/questao4.pl
+++ lab4/questao4.pl
@@ -1,13 +1,22 @@
-passaro(joao).
-peixe(pedro).
-minhoca(maria).
-gosta(passaro, minhoca).
-gosta(gato, peixe).
 
-amigo(X, Y) :- gosta(X, Y), gosta(Y, X).
+animal(joao, passaro).
+animal(pedro, peixe).
+animal(maria, minhoca).
+animal(miau, gato).
+animal(eu, pessoa).
 
-amigo(gato, eu).
+verifica_gosto(passaro,minhoca).
+verifica_gosto(gato, peixe).
+verifica_gosto(gato, passaro).
 
-come(gato, X) :- gosta(gato, X), not pessoa(X).
 
-gato(miau).
+gosta(Nome1, Nome2) :- animal(Nome1,Animal1), animal(Nome2, Animal2) Animal1 =:= gato, Animal2 =\= pessoa.
+gosta(Nome1,Nome2) :- animal(Nome1,Animal1), animal(Nome2, Animal2), verifica_gosto(Animal1, Animal2).
+
+
+
+amigo(Nome1, Nome2).
+
+
+come(Nome1, Nome2) :- gosta(Nome1, Nome2).
+come(Nome1, Nome2) :- amigo(Nome1, Nome2).
